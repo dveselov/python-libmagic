fc4d4bc29db7f5ff5ac202740262a823cc06519c
meowie <trex@require.pm>
2014-07-18 03:03:32 +0400

    Initial magic object

--- magic/__init__.py
+++ magic/__init__.py
@@ -1,4 +1,39 @@
-import ffi
+from . import ffi
+from . import flags
 
-__version__ = "0.1"
+__version__ = "0.1.0"
 
+
+class Magic(object):
+
+  def __init__(self, mime=False, debug=False, database=None):
+    initial_flags = flags.MAGIC_NONE
+    if mime:
+      initial_flags |= flags.MAGIC_MIME_TYPE
+    if debug:
+      initial_flags |= flags.MAGIC_DEBUG
+    cookie = ffi.open(initial_flags)
+    if database:
+      ffi.load(cookie, database)
+    else:
+      ffi.load(cookie)
+    self.cookie = cookie
+
+  def __del__(self):
+    try:
+      ffi.close(self.cookie)
+    except Exception as exception:
+      raise
+
+  @property
+  def version(self):
+    return ffi.version()
+
+  def set_flags(self, flags):
+    return ffi.set_flags(self.cookie, flags)
+
+  def from_file(self, filepath):
+    return ffi.file(self.cookie, filepath)
+
+  def from_buffer(self, value):
+    return ffi.buffer(self.cookie, value)
