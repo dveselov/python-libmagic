2a492a8cc47eccdae06a7df691b28e12196f19d5
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-07-07 06:50:01 +0300

    Update CFFI to 1.7.0

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from distutils.core import setup
 
 setup(**{
-  'name': 'python-libmagic',
-  'packages': ['magic'],
-  'url': 'https://github.com/tyrannosaurus/python-libmagic',
-  'author': 'python-libmagic contributors',
-  'author_email': 'github@require.pm',
-  'version': '0.3.0',
-  'description': 'Python bindings to libmagic',
-  'license': 'MIT',
-  'classifiers': (
-    'Intended Audience :: Developers',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Programming Language :: Python :: Implementation :: PyPy',
+  "name": "python-libmagic",
+  "packages": ["magic"],
+  "url": "https://github.com/dveselov/python-libmagic",
+  "author": "Dmitry Veselov",
+  "author_email": "d.a.veselov@yandex.ru",
+  "version": "0.3.0",
+  "description": "Python bindings to libmagic",
+  "license": "MIT",
+  "classifiers": (
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
   ),
-  'install_requires': [
-    'cffi==0.8',
+  "install_requires": [
+    "cffi==1.7.0",
   ],
 })
